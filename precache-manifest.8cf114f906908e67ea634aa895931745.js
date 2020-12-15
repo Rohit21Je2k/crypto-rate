680cf7f1612d100f447e8b12a4b34c3568f5b081
Steve <you@example.com>
2020-12-16 00:09:58 +0530

    Updates

---
+++ precache-manifest.8cf114f906908e67ea634aa895931745.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce9302160f57f11c35f04e7e049add67",
+    "url": "/Rohit21Je2k/crypto-rate/index.html"
+  },
+  {
+    "revision": "8c683716af1ebeb98809",
+    "url": "/Rohit21Je2k/crypto-rate/static/css/main.1fc47c9d.chunk.css"
+  },
+  {
+    "revision": "7026b005810f335b5e7c",
+    "url": "/Rohit21Je2k/crypto-rate/static/js/2.74944c8f.chunk.js"
+  },
+  {
+    "revision": "a3ced764de97f5762acff50c1b0dceba",
+    "url": "/Rohit21Je2k/crypto-rate/static/js/2.74944c8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c683716af1ebeb98809",
+    "url": "/Rohit21Je2k/crypto-rate/static/js/main.cec49eab.chunk.js"
+  },
+  {
+    "revision": "e6ebe2a9df1d9b9eaefe",
+    "url": "/Rohit21Je2k/crypto-rate/static/js/runtime-main.57645f31.js"
+  },
+  {
+    "revision": "c0179f278df3efce556fe16e72d93701",
+    "url": "/Rohit21Je2k/crypto-rate/static/media/background-image.c0179f27.jpg"
+  }
+]);
